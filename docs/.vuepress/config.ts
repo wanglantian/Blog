e2009495eb3a54e120bdd8d8d8011735892171b4
18015566240 <2497533962@qq.com>
2022-04-29 13:00:43 +0800

    111

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       description: 'DotNet,Nodejs,Vue',
     }
   },
-  base: '/blog/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '/Blog/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
